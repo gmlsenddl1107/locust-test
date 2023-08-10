c4ebaf42e0f348b28c291f5b81021308d8809649
DESKTOP-8ATURHM\min <ardentgirl7@naver.com>
2023-08-10 16:15:48 +0900

    오류 수정

--- main.py
+++ main.py
@@ -3,32 +3,31 @@ import argparse
 import os
 import copy
 import pandas as pd
+
+
 def merge_result(result_path):
-    final =[]
-    col=None
-    is_make_column=False
+    final = []
+    col = None
+    is_make_column = False
     for file_path in os.listdir(result_path):
 
         if "_stats.py" in os.path.basename(file_path):
             try:
-                data= pd.read_csv(file_path)
+                data = pd.read_csv(file_path)
                 if not is_make_column:
-                    col= list(data.columns[1:])
-                    col[0]="vuser"
-                    is_make_column=True
+                    col = list(data.columns[1:])
+                    col[0] = "vuser"
+                    is_make_column = True
 
-                final.append( copy.deepcopy(data[-1][1:]))
+                final.append(copy.deepcopy(data[-1][1:]))
             except Exception as e:
                 print(e)
             else:
                 is_make_column = True
 
-
-
-    final_result = pd.DataFrame(final,columns=col)
-    save_filename ="final_result_"+os.path.basename(result_path)+".csv"
-    final_result.to_csv(result_path/save_filename)
-
+    final_result = pd.DataFrame(final, columns=col)
+    save_filename = "final_result_" + os.path.basename(result_path) + ".csv"
+    final_result.to_csv(result_path / save_filename)
 
 
 def create_folder_if_not_exists(folder_path):
@@ -37,7 +36,8 @@ def create_folder_if_not_exists(folder_path):
     else:
         os.makedirs(folder_path)
 
-def make_env_file(locustfile_full_path,target_address,users,test_name,version):
+
+def make_env_file(locustfile_full_path, target_address, users, test_name, version):
     if os.path.exists("./.env"):
         os.remove("./.env")
     with open("./.env", 'w') as f:
@@ -46,29 +46,31 @@ def make_env_file(locustfile_full_path,target_address,users,test_name,version):
         f.write(f"USERS={users}\n")
         f.write(f"TEST_NAME={test_name}\n")
         f.write(f"VERSION={version}\n")
+
+
 def run_docker_compose():
     import subprocess
-    cmd = ['docker-compose', 'up', '--scale','worker=4','force-recreate']
+    cmd = ['docker-compose', 'up', '--scale', 'worker=4', '--force-recreate']
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     for line in p.stdout:
-        print( line)
+        print(line)
     p.wait()
     print(p.returncode)
     print("--------end---------")
     cmd = ['docker-compose', 'donw']
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     for line in p.stdout:
-        print( line)
+        print(line)
     p.wait()
     print(p.returncode)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('version', type=str)
-    parser.add_argument('locustfile_name_in_locustfiles', type=str)
-    parser.add_argument('target_address', type=str,help="172.20.93.18:8888")
-    parser.add_argument('use_warm_up', type=str, default=False)
+    parser.add_argument('--version', type=str)
+    parser.add_argument('--locustfile_name_in_locustfiles', type=str)
+    parser.add_argument('--target_address', type=str, help="172.20.93.18:8888")
+    parser.add_argument('--use_warm_up', type=str, default=False, required=False)
 
     args = parser.parse_args()
 
@@ -78,8 +80,7 @@ if __name__ == "__main__":
     create_folder_if_not_exists(result_folder_path)
     if args.use_warm_up:
         make_env_file(locustfile_full_path, args.target_address, 512, test_name, args.version)
-    for users in [1,32,64,128,256,512]:
-        make_env_file(locustfile_full_path,args.target_address,users,test_name,args.version)
+    for users in [1, 32, 64, 128, 256, 512]:
+        make_env_file(locustfile_full_path, args.target_address, users, test_name, args.version)
         run_docker_compose()
     merge_result(result_folder_path)
-
